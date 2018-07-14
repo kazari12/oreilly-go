c6dc12c71d69b6e36144c0c5710ff7e87c888a12
kazari <kazoo.g@gmail.com>
2018-07-14 16:35:08 +0900

    p17

--- chat/main.go
+++ chat/main.go
@@ -3,23 +3,34 @@ package main
 import (
   "log"
   "net/http"
+  "sync"
+  "html/template"
+  "path/filepath"
 )
 
-func main() {
-  http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-  w.Write([]byte(`
-    <html>
-      <head>
-        <title>チャット</title>
-      </head>
-      <body>
-        チャットしましょう！
-      </body>
-    </html>
-    `))
-  })
+//templは１つのテンプレートを表します
+type templateHandler struct{
+  once  sync.Once
+  filename string
+  templ    *template.Template
+}
+//SergveHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  t.once.Do(func() {
+    t.templ =
+      template.Must(template.ParseFiles(filepath.Join("templates",
+        t.filename)))
+      })
+t.templ.Execute(w,nil)
+}
 
-  //Webサーバを開始
+func main() {
+  r := newRoom()
+  http.Handle("/", &templateHandler{filename: "chat.html"})
+  http.Handle("/room", r)
+  //チャットルームを開始します
+  go r.run()
+  //Webサーバを開始します
   if err := http.ListenAndServe(":8080", nil); err != nil{
     log.Fatal("ListenAndServe:", err)
   }
